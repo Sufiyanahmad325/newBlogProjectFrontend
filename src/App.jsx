0a0ffb790b3858f9eeead655d6eb0d911ca7aeb3
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-10-04 13:08:12 +0530

    fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import { createContext, useState } from 'react'
 import './App.css'
 import Header from './MainPage/Header'
-import TwoBigCardBelowHader from './MainPage/TwoBigCardBelowHader'
-import BlogCard from './MainPage/BlogCard'
-import LoginForm from './MainPage/LoginForm'
 import { Outlet } from 'react-router-dom'
+import LoginForm from './MainPage/LoginForm'
 
 export const BlogContext = createContext()
 function App() {
   const [openLoingForm, setOpenLoingForm] = useState(false) // default false
 
   return (
-    <BlogContext.Provider value={{openLoingForm, setOpenLoingForm}}>
-    <div className="">
-      <Header  />
-      <Outlet />
-    </div>
+    <BlogContext.Provider value={{ openLoingForm, setOpenLoingForm }}>
+      <div className="">
+        <Header />
+        <Outlet />
+        <LoginForm />
+      </div>
     </BlogContext.Provider>
   )
 }
