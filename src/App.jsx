6a27229430dc483d52fcdff2c63d0558db567946
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-10-05 11:41:54 +0530

    fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import './App.css'
 import Header from './MainPage/Header'
 import { Outlet } from 'react-router-dom'
 import LoginForm from './MainPage/LoginForm'
+import Signup from './SignUp'
 
 export const BlogContext = createContext()
 function App() {
   const [openLoingForm, setOpenLoingForm] = useState(false) // default false
+  const [signUp, setSignUp] = useState(false)
 
   return (
-    <BlogContext.Provider value={{ openLoingForm, setOpenLoingForm }}>
+    <BlogContext.Provider value={{ openLoingForm, setOpenLoingForm  ,setSignUp , signUp}}>
       <div className="">
         <Header />
         <Outlet />
         <LoginForm />
+        <Signup />
       </div>
     </BlogContext.Provider>
   )
