1fef0dd14616d3a6e6b1e027e70f91131494e126
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-10-08 15:55:40 +0530

    front has benn added successfully from backend

--- src/MainPage/BlogCard.jsx
+++ src/MainPage/BlogCard.jsx
@@ -1,12 +1,19 @@
 // BlogCards.jsx
-import React from "react";
+import React, { use, useContext, useEffect, useState } from "react";
+import { BlogContext } from "../App";
 
 const BlogCards = () => {
+  const { userAllBlog} = useContext(BlogContext)
+
+  
+ 
+
+
   return (
     <div className="container mx-auto px-16 mt-8">
       {/* Blog Cards Section */}
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
-        {[1, 2, 3, 4, 5, 6,7,8].map((card, i) => (
+        {userAllBlog?.map((blog, i) => (
           <div
             key={i}
             className="rounded-xl shadow-md p-4 bg-white hover:shadow-lg transition"
@@ -16,12 +23,12 @@ const BlogCards = () => {
               alt="Blog"
               className="w-full h-40 object-cover rounded-md mb-3"
             />
-            <h3 className="font-semibold text-lg">Blog Title {i + 1}</h3>
+            <h3 className="font-semibold text-lg">{blog.title || 'title Not Availble' }</h3>
             <p className="text-sm text-gray-500 mt-1">
-              Author Name • {new Date().toLocaleDateString()}
+               Date • {new Date().toLocaleDateString()}
             </p>
             <p className="text-gray-600 text-sm mt-2">
-              Short description of the blog post goes here...
+              {blog.content || 'content Not Availble' }
             </p>
           </div>
         ))}
